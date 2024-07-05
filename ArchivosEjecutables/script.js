411b6e562763ed098d196cb3f00c8408d75aad9f
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-05 13:45:47 -0400

    Falta agregar más detalles al archivo .css

--- ArchivosEjecutables/script.js
+++ ArchivosEjecutables/script.js
@@ -2,4 +2,16 @@ function enableDarkMode()
 {
     let main_body = document.body;
     main_body.classList.toggle("dark-mode");
+
+    // REVISA SI EL COMPONENTE ASIGNADO ESTÁ COMPROBADO O NO.
+
+    if (document.getElementById('checkitem').checked)
+    {
+        console.log("checked");
+    }
+    else
+    {
+        console.log("NOT checked");
+
+    }
 }
